35100a1e9cc882cf85cc66fa19479de734b55ad6
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-01-31 19:41:29 +0200

    Avoid defining macros starting with underscore under clang

--- global.hpp
+++ global.hpp
@@ -5,14 +5,18 @@
 #include "config.hpp"
 
 #ifdef DEBUG
+#ifndef __clang__
 #define _GLIBCXX_DEBUG
 #define _GLIBCXX_DEBUG_PEDANTIC
+#endif
 #else
+#ifndef NDEBUG
 #define NDEBUG
+#endif
 #define GSL_UNENFORCED_ON_CONTRACT_VIOLATION
 #endif
 
-#if defined(__has_feature)
+#if defined(__has_feature) && !defined(__clang__)
 #if __has_feature(address_sanitizer)
 #define _GLIBCXX_SANITIZE_VECTOR 1
 #endif

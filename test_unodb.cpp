31dca59fe29e7c722119d6c119942f16f1211c11
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-02-01 20:35:52 +0200

    Add a test for single node tree with value comparison

--- test_unodb.cpp
+++ test_unodb.cpp
@@ -1,10 +1,16 @@
 // Copyright 2019 Laurynas Biveinis
-#include "art.hpp"
+
+#include "global.hpp"
+
+#include <algorithm>
+
 #include "gtest/gtest.h"  // IWYU pragma: keep
+
+#include "art.hpp"
 
 namespace {
 
-TEST(UnoDB, single_node_tree) {
+TEST(UnoDB, single_node_tree_empty_value) {
   unodb::db test_db;
   auto result = test_db.get(1);
   ASSERT_TRUE(!result);
@@ -13,6 +19,17 @@ TEST(UnoDB, single_node_tree) {
   ASSERT_TRUE(!result);
   result = test_db.get(1);
   ASSERT_TRUE(result);
+}
+
+TEST(UnoDB, single_node_tree_nonempty_value) {
+  std::array<std::byte, 3> test_value = {std::byte{0x03}, std::byte{0x00},
+                                         std::byte{0x01}};
+  unodb::db test_db;
+  test_db.insert(1, unodb::value_view{test_value});
+  const auto result = test_db.get(1);
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(std::equal(result->cbegin(), result->cend(), test_value.cbegin(),
+                         test_value.cend()));
 }
 
 }  // namespace

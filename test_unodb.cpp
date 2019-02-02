3b31b6b7bc4e4c5513457f18604018f24e7295bf
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-02-02 16:13:36 +0200

    Extract out a helper test value and helper test function

--- test_unodb.cpp
+++ test_unodb.cpp
@@ -10,6 +10,16 @@
 
 namespace {
 
+const constexpr auto test_value_3 =
+    std::array<std::byte, 3>{std::byte{0x03}, std::byte{0x00}, std::byte{0x01}};
+
+auto assert_result_eq(unodb::db::get_result result,
+                      unodb::value_view expected) noexcept {
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(std::equal(result->cbegin(), result->cend(), expected.cbegin(),
+                         expected.cend()));
+}
+
 TEST(UnoDB, single_node_tree_empty_value) {
   unodb::db test_db;
   auto result = test_db.get(1);
@@ -22,14 +32,10 @@ TEST(UnoDB, single_node_tree_empty_value) {
 }
 
 TEST(UnoDB, single_node_tree_nonempty_value) {
-  std::array<std::byte, 3> test_value = {std::byte{0x03}, std::byte{0x00},
-                                         std::byte{0x01}};
   unodb::db test_db;
-  test_db.insert(1, unodb::value_view{test_value});
+  test_db.insert(1, unodb::value_view{test_value_3});
   const auto result = test_db.get(1);
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(std::equal(result->cbegin(), result->cend(), test_value.cbegin(),
-                         test_value.cend()));
+  assert_result_eq(result, test_value_3);
 }
 
 }  // namespace

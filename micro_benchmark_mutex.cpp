0a5a2d05dee1c89ebd35b36d29492b06a685c427
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-01-24 19:47:08 +0200

    Fix multithreaded use of Google Benchmark
    Since the workload being tested uses threads internally, it is a mistake to use
    Google Benchmark Threads parameter, as that results in only one thread being
    benchmarked, and, since its work share is lower, nearly linear apparent
    scalability.
    
    Fix by moving thread handling to the benchmark loop itself. Reduce the maximum
    number of the threads being benchmarked because scalability limit now becomes
    apparent much sooner.

--- micro_benchmark_mutex.cpp
+++ micro_benchmark_mutex.cpp
@@ -2,100 +2,139 @@
 
 #include "global.hpp"
 
+#include <thread>
+
+#include <benchmark/benchmark.h>
+
 #include "art.hpp"
 #include "micro_benchmark.hpp"
 #include "mutex_art.hpp"
-
-#include <benchmark/benchmark.h>
 
 namespace {
 
 std::unique_ptr<unodb::mutex_db> test_db;
 
-constexpr auto parallel_get_tree_size = 10000000;
+// Do not swap on my 16GB RAM laptop
+constexpr auto parallel_get_tree_size = 7000000;
 
-void parallel_get(benchmark::State &state) {
-  if (state.thread_index == 0) {
-    test_db = std::make_unique<unodb::mutex_db>();
-    for (unodb::key i = 0; i < static_cast<unodb::key>(parallel_get_tree_size);
-         ++i) {
-      (void)test_db->insert(i, values[i % values.size()]);
-    }
-  }
-
-  const auto length = parallel_get_tree_size / state.threads;
-  const auto start = state.thread_index * length;
-  for (auto _ : state) {
-    for (unodb::key i = static_cast<unodb::key>(start);
-         i < static_cast<unodb::key>(start + length); ++i) {
-      benchmark::DoNotOptimize(test_db->get(i));
-    }
-  }
-
-  if (state.thread_index == 0) {
-    test_db.reset(nullptr);
+void parallel_get_worker(unodb::key start, unodb::key length) {
+  for (unodb::key i = start; i < start + length; ++i) {
+    benchmark::DoNotOptimize(test_db->get(i));
   }
 }
 
-constexpr auto parallel_insert_tree_size = 10000000;
-
-void parallel_insert_disjoint_ranges(benchmark::State &state) {
-  if (state.thread_index == 0) {
-    test_db = std::make_unique<unodb::mutex_db>();
+void parallel_get(benchmark::State &state) {
+  test_db = std::make_unique<unodb::mutex_db>();
+  for (unodb::key i = 0; i < static_cast<unodb::key>(parallel_get_tree_size);
+       ++i) {
+    (void)test_db->insert(i, values[i % values.size()]);
   }
 
-  const auto length = parallel_insert_tree_size / state.threads;
-  const auto start = state.thread_index * length;
+  const std::size_t num_of_threads = static_cast<std::size_t>(state.range());
+  const unodb::key length = parallel_get_tree_size / num_of_threads;
+  std::vector<std::thread> threads{num_of_threads};
+
   for (auto _ : state) {
-    for (unodb::key i = static_cast<unodb::key>(start);
-         i < static_cast<unodb::key>(start + length); ++i) {
-      benchmark::DoNotOptimize(test_db->insert(i, values[i % values.size()]));
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      const unodb::key start = i * length;
+      threads[i] = std::thread{parallel_get_worker, start, length};
+    }
+
+    parallel_get_worker(0, length);
+
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      threads[i].join();
     }
   }
 
-  if (state.thread_index == 0) {
-    test_db.reset(nullptr);
+  test_db.reset(nullptr);
+}
+
+constexpr auto parallel_insert_tree_size = 7000000;
+
+void parallel_insert_worker(unodb::key start, unodb::key length) {
+  for (unodb::key i = start; i < start + length; ++i) {
+    benchmark::DoNotOptimize(test_db->insert(i, values[i % values.size()]));
   }
+}
+
+void parallel_insert_disjoint_ranges(benchmark::State &state) {
+  test_db = std::make_unique<unodb::mutex_db>();
+
+  const std::size_t num_of_threads = static_cast<std::size_t>(state.range());
+  const unodb::key length = parallel_insert_tree_size / num_of_threads;
+  std::vector<std::thread> threads{num_of_threads};
+
+  for (auto _ : state) {
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      const unodb::key start = i * length;
+      threads[i] = std::thread{parallel_insert_worker, start, length};
+    }
+
+    parallel_insert_worker(0, length);
+
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      threads[i].join();
+    }
+  }
+
+  test_db.reset(nullptr);
 }
 
 constexpr auto parallel_delete_tree_size = 10000000;
 
+void parallel_delete_worker(unodb::key start, unodb::key length) {
+  for (unodb::key i = start; i < start + length; ++i) {
+    benchmark::DoNotOptimize(test_db->remove(i));
+  }
+}
+
 void parallel_delete_disjoint_ranges(benchmark::State &state) {
-  if (state.thread_index == 0) {
-    test_db = std::make_unique<unodb::mutex_db>();
-    for (unodb::key i = 0; i < static_cast<unodb::key>(parallel_get_tree_size);
-         ++i) {
-      (void)test_db->insert(i, values[i % values.size()]);
-    }
+  test_db = std::make_unique<unodb::mutex_db>();
+  for (unodb::key i = 0; i < static_cast<unodb::key>(parallel_delete_tree_size);
+       ++i) {
+    (void)test_db->insert(i, values[i % values.size()]);
   }
 
-  const auto length = parallel_delete_tree_size / state.threads;
-  const auto start = state.thread_index * length;
+  const std::size_t num_of_threads = static_cast<std::size_t>(state.range());
+  const unodb::key length = parallel_delete_tree_size / num_of_threads;
+  std::vector<std::thread> threads{num_of_threads};
+
   for (auto _ : state) {
-    for (unodb::key i = static_cast<unodb::key>(start);
-         i < static_cast<unodb::key>(start + length); ++i) {
-      benchmark::DoNotOptimize(test_db->remove(i));
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      const unodb::key start = i * length;
+      threads[i] = std::thread{parallel_delete_worker, start, length};
+    }
+
+    parallel_delete_worker(0, length);
+
+    for (std::size_t i = 1; i < num_of_threads; ++i) {
+      threads[i].join();
     }
   }
 
-  if (state.thread_index == 0) {
-    test_db.reset(nullptr);
-  }
+  test_db.reset(nullptr);
 }
 
 }  // namespace
 
 BENCHMARK(parallel_get)
-    ->ThreadRange(1, 4096)
+    ->RangeMultiplier(2)
+    ->Range(1, 16)
     ->Unit(benchmark::kMillisecond)
+    ->MeasureProcessCPUTime()
     ->UseRealTime();
 BENCHMARK(parallel_insert_disjoint_ranges)
-    ->ThreadRange(1, 512)
+    ->RangeMultiplier(2)
+    ->Range(1, 32)
     ->Unit(benchmark::kMillisecond)
+    ->MeasureProcessCPUTime()
     ->UseRealTime();
 BENCHMARK(parallel_delete_disjoint_ranges)
-    ->ThreadRange(1, 1024)
+    ->RangeMultiplier(2)
+    ->Range(1, 32)
     ->Unit(benchmark::kMillisecond)
+    ->MeasureProcessCPUTime()
     ->UseRealTime();
 
 BENCHMARK_MAIN();

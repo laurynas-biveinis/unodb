727a6536680907385202ef70525af0e9949a48fd
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-01-29 19:25:30 +0200

    cmake-build.el: add GCC 7 & 8 build configurations

--- .cmake-build.el
+++ .cmake-build.el
@@ -2,7 +2,13 @@
   (release "-DCMAKE_BUILD_TYPE=Release")
   (debug "-DCMAKE_BUILD_TYPE=Debug")
   (debug-asan "-DCMAKE_BUILD_TYPE=Debug -DSANITIZE=ON")
-  (debug-tsan "-DCMAKE_BUILD_TYPE=Debug -DSANITIZE_THREAD=ON"))
+  (debug-tsan "-DCMAKE_BUILD_TYPE=Debug -DSANITIZE_THREAD=ON")
+
+  (gcc7-release "-DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7")
+  (gcc7-debug "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7")
+
+  (gcc8-release "-DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8")
+  (gcc8-debug "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8"))
 
  (cmake-build-run-configs
   (test

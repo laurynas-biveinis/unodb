e1f461026f6a6c6fb47f1ec5dbc55e4abea7a876
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-01-19 08:40:38 +0200

    Fix ispell-personal-dictionary setting [no ci]

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,11 @@
-((nil . ((ispell-personal-dictionary . ".ispell.dict")))
+;; StackOverflow'ed from https://stackoverflow.com/a/11474657/80458
+;; An alternative would be to use `projectile-project-root', but the current
+;; method works without Projectile too.
+((nil . ((eval . (setq ispell-personal-dictionary
+                       (expand-file-name
+                        ".ispell.dict"
+                        (file-name-directory
+                         (let ((d (dir-locals-find-file "./")))
+                           (if (stringp d) d (car d)))))))))
  (c++-mode . ((c-tab-always-indent t)
               (c-file-style . "Google"))))

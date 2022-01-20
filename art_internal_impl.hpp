a8bd4e972e8d827f0c0f067d1f4667bbd36e7db8
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-01-20 05:26:08 +0200

    Add ARM NEON implementation for Node16 insert position search

--- art_internal_impl.hpp
+++ art_internal_impl.hpp
@@ -17,6 +17,8 @@
 #ifdef UNODB_DETAIL_X86_64
 #include <emmintrin.h>
 #include <smmintrin.h>
+#elif defined(__aarch64__)
+#include <arm_neon.h>
 #endif
 
 #include <gsl/gsl_util>
@@ -54,6 +56,23 @@ basic_art_key<std::uint64_t>::make_binary_comparable(std::uint64_t k) noexcept {
 }
 
 #else  // #ifdef UNODB_DETAIL_X86_64
+
+#ifdef __aarch64__
+
+// https://stackoverflow.com/a/58381188/80458
+[[nodiscard, gnu::const]] inline int vmovmaskq_u8(uint8x16_t x) noexcept {
+  uint16x8_t high_bits = vreinterpretq_u16_u8(vshrq_n_u8(x, 7));
+  uint32x4_t paired16 =
+      vreinterpretq_u32_u16(vsraq_n_u16(high_bits, high_bits, 7));
+  uint64x2_t paired32 =
+      vreinterpretq_u64_u32(vsraq_n_u32(paired16, paired16, 14));
+  uint8x16_t paired64 =
+      vreinterpretq_u8_u64(vsraq_n_u64(paired32, paired32, 28));
+  return vgetq_lane_u8(paired64, 0) |
+         (static_cast<int>(vgetq_lane_u8(paired64, 8)) << 8);
+}
+
+#endif  // #ifdef __aarch64__
 
 // From public domain
 // https://graphics.stanford.edu/~seander/bithacks.html
@@ -1337,12 +1356,23 @@ class basic_inode_16 : public basic_inode_16_parent<ArtPolicy> {
         static_cast<unsigned>(_mm_movemask_epi8(lesser_key_positions)) & mask;
     const auto result = static_cast<std::uint8_t>(
         (bit_field != 0) ? detail::ctz(bit_field) : children_count_);
-#else
+#elif defined(__aarch64__)
+    const auto replicated_search_key =
+        vdupq_n_u8(static_cast<std::uint8_t>(key_byte));
+    const auto lesser_key_positions =
+        vcleq_u8(replicated_search_key, keys.byte_vector);
+    const auto mask = (1U << children_count_) - 1;
+    const auto bit_field =
+        static_cast<unsigned>(detail::vmovmaskq_u8(lesser_key_positions)) &
+        mask;
+    const auto result = static_cast<std::uint8_t>(
+        (bit_field != 0) ? detail::ctz(bit_field) : children_count_);
+#else   // #elif defined(__aarch64__)
     const auto result = static_cast<std::uint8_t>(
         std::lower_bound(keys.byte_array.cbegin(),
                          keys.byte_array.cbegin() + children_count_, key_byte) -
         keys.byte_array.cbegin());
-#endif
+#endif  // #elif defined(__aarch64__)
 
     UNODB_DETAIL_ASSERT(
         result == children_count_ ||
@@ -1354,9 +1384,13 @@ class basic_inode_16 : public basic_inode_16_parent<ArtPolicy> {
   union key_union {
     std::array<critical_section_policy<std::byte>, basic_inode_16::capacity>
         byte_array;
+
 #ifdef UNODB_DETAIL_X86_64
     __m128i byte_vector;
+#elif defined(__aarch64__)
+    uint8x16_t byte_vector;
 #endif
+
     key_union() noexcept {}
   } keys;
   std::array<critical_section_policy<node_ptr>, basic_inode_16::capacity>

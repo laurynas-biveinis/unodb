da2c7ab42c16d0f43aa248bc6b9f7bbee98a8d94
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-11-06 16:55:38 +0200

    Emacs .dir-locals.el: make ispell-personal-dictionary buffer-local variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,13 @@
 ;; StackOverflow'ed from https://stackoverflow.com/a/11474657/80458
 ;; An alternative would be to use `projectile-project-root', but the current
 ;; method works without Projectile too.
-((nil . ((eval . (setq ispell-personal-dictionary
-                       (expand-file-name
-                        ".ispell.dict"
-                        (file-name-directory
+((nil . ((eval . (progn
+                   (make-local-variable 'ispell-personal-dictionary)
+                   (setq ispell-personal-dictionary
+                         (expand-file-name
+                         ".ispell.dict"
+                         (file-name-directory
                          (let ((d (dir-locals-find-file "./")))
-                           (if (stringp d) d (car d)))))))))
+                           (if (stringp d) d (car d))))))))))
  (c++-mode . ((c-tab-always-indent t)
               (c-file-style . "Google"))))

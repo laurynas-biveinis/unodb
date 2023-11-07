b6db1f3e93317bc35b2986b9568d6008c67d36d1
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-11-07 13:47:03 +0200

    .dir-locals.el: simplify make-local-variable + setq -> setq-local

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,13 +1,11 @@
 ;; StackOverflow'ed from https://stackoverflow.com/a/11474657/80458
 ;; An alternative would be to use `projectile-project-root', but the current
 ;; method works without Projectile too.
-((nil . ((eval . (progn
-                   (make-local-variable 'ispell-personal-dictionary)
-                   (setq ispell-personal-dictionary
-                         (expand-file-name
-                         ".ispell.dict"
-                         (file-name-directory
-                         (let ((d (dir-locals-find-file "./")))
-                           (if (stringp d) d (car d))))))))))
+((nil . ((eval . (setq-local ispell-personal-dictionary
+                             (expand-file-name
+                              ".ispell.dict"
+                              (file-name-directory
+                               (let ((d (dir-locals-find-file "./")))
+                                 (if (stringp d) d (car d)))))))))
  (c++-mode . ((c-tab-always-indent t)
               (c-file-style . "Google"))))

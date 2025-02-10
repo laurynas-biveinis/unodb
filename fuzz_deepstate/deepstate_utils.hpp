c73a74192e1ece3907381d175163067e99492924
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2025-02-10 08:14:16 +0200

    Make DeepState tests honor the timeouts passed on command line
    The DeepState harness checks for timeouts between the test runs, which are very
    long in our tests. Before the fix, the timeouts were effectively ignored for the
    ART fuzzer, resulting in Valgrind timeouts in CI.

--- fuzz_deepstate/deepstate_utils.hpp
+++ fuzz_deepstate/deepstate_utils.hpp
@@ -1,10 +1,11 @@
-// Copyright 2021 Laurynas Biveinis
+// Copyright 2021-2025 Laurynas Biveinis
 #ifndef UNODB_DETAIL_DEEPSTATE_UTILS_HPP
 #define UNODB_DETAIL_DEEPSTATE_UTILS_HPP
 
 #include "global.hpp"
 
 #include <cstddef>
+#include <ctime>
 
 #include <deepstate/DeepState.hpp>
 
@@ -23,5 +24,27 @@ template <class T>
   ASSERT(!container.empty());
   return DeepState_SizeTInRange(0, std::size(container) - 1);
 }
+
+/// The DeepState command line-specified timeout in seconds. We need it, but
+/// it is not exposed through the public DeepState API, hence take the risk
+/// and declare it ourselves.
+extern "C" int FLAGS_timeout;
+
+namespace unodb::test {
+
+/// Check whether the DeepState test timeout has been reached.
+/// \param[in] start_tm Test start timestamp in seconds since epoch
+/// \return true if current time exceeds \a start_tm by more than the timeout
+/// value.
+///
+/// The timeout value is specified via DeepState command line. Since the test
+/// harness only checks it between the tests, we need additional checks for
+/// long-running tests.
+[[nodiscard]] bool inline timeout_reached(std::time_t start_tm) {
+  const auto current_tm = std::time(nullptr);
+  return current_tm - start_tm > FLAGS_timeout;
+}
+
+}  // namespace unodb::test
 
 #endif  // UNODB_DETAIL_DEEPSTATE_UTILS_HPP
